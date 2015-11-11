b63ccaa1ecb4e45a08bf6df9e53a1b53814f9472
Steve Kinney <hello@stevekinney.net>
2015-11-11 15:41:47 -0700

    Add redirect

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const crypto = require('crypto');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -11,9 +12,11 @@ const path = require('path');
 
 app.use(express.static('public'));
 
+const polls = {};
+
 app.get('/', function (req, res){
   res.send(`<html><body>
-    <form action="/poll/new" method="post">
+    <form action="/polls/new" method="post">
       <label for="name">Title</label><br>
       <input id="name" type="text" name="poll[title]"><br>
       <hr>
@@ -25,9 +28,23 @@ app.get('/', function (req, res){
     </body></html>`);
 });
 
-app.post('/poll/new', function (req, res) {
-  console.log(req.body);
-  res.send('You submited: ' + JSON.stringify(req.body) + '.');
+app.get('/polls/:id', function (req, res) {
+  var poll = polls[req.params.id];
+  res.send(`<html><body>
+      <h1>${poll.title}</h1>
+    </body></html>`);
+});
+
+app.post('/polls/new', function (req, res) {
+  var poll = req.body.poll;
+  var id = crypto.randomBytes(20).toString('hex');
+
+  poll.id = id;
+  poll.voterId = crypto.randomBytes(20).toString('hex');
+  poll.voterUrl = '/vote/' + voterId;
+
+  polls[id] = poll;
+  res.redirect('/polls/' + id);
 });
 
 http.listen(process.env.PORT || 3000, function(){
